95874d2a1c4114b01861a2730ef8dcbe26eda950
LuccasLukaDev <luccaslukadev@gmail.com>
2026-09-04 10:08:32 -0300

    fix: dlksfjsdlkjf

--- frontend/FleetOps/src/components/App.tsx
+++ frontend/FleetOps/src/components/App.tsx
@@ -46,7 +46,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p>Your questioadakjshdkashdkjadns, answered</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
