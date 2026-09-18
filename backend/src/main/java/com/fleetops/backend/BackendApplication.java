0b57099f3e320f444ce01286c7d156fd14b22460
Hugo leo <hugoleonardo040906@gmail.com>
2026-09-18 18:11:44 -0300

    merge: Atualizando essa branch com a Staged

--- backend/src/main/java/com/fleetops/backend/BackendApplication.java
+++ backend/src/main/java/com/fleetops/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication()
 public class BackendApplication {
 
     public static void main(String[] args) {
